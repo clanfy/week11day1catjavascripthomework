baa10a11c23f1f905eb8411d28827f44813c5896
Clacson <cookiellacson@gmail.com>
2016-12-05 22:12:09 +0000

    edits append order to correct formatting

--- public/app.js
+++ public/app.js
@@ -9,13 +9,14 @@ window.onload = function(){
   var catFood = document.createElement('li');
   catFood.innerText = "Favorite food: Bin Bags";
 
-  var catPicture = document.createElement("img");
-  catPicture.setAttribute('src', 'http://www.catbreedselector.com/wp-content/uploads/2016/09/Black-Burmese-Cat.jpg');
+  var catPicture = document.createElement('img');
+  catPicture.setAttribute('src','http://www.catbreedselector.com/wp-content/uploads/2016/09/Black-Burmese-Cat.jpg');
   catPicture.setAttribute('width', 500);
+  // document.getElementById('img').appendChild(catPicture);
 
-  catFood.appendChild(catPicture);
-  catName.appendChild(catFood);
-  catList.appendChild(catName);
+  catList.append(catName);
+  catList.append(catFood);
+  catList.append(catPicture);
 
   var catStuff = document.querySelector('#cats');
   catStuff.append(catList);
