4db8872b57a288981dd49a6e9b2e6bf7bb8c326d
Clacson <cookiellacson@gmail.com>
2016-12-05 21:45:49 +0000

    adds Buddy the cat programatically rough and ready

--- public/app.js
+++ public/app.js
@@ -4,13 +4,14 @@ window.onload = function(){
   catList.classList.add('cat');
 
   var catName = document.createElement('li');
-  catName.innerText = "Name: Buddy";
+  catName.innerText = "Name: Buddy (RIP)";
 
   var catFood = document.createElement('li');
-  catFood.innerText = "Favorite food: bin bags";
+  catFood.innerText = "Favorite food: Bin Bags";
 
-  var catPicture = document.createElement('li');
-  catPicture.innerText = "picture stuff";
+  var catPicture = document.createElement("img");
+  catPicture.setAttribute('src', 'http://www.catbreedselector.com/wp-content/uploads/2016/09/Black-Burmese-Cat.jpg');
+  catPicture.setAttribute('width', 500);
 
   catFood.appendChild(catPicture);
   catName.appendChild(catFood);
