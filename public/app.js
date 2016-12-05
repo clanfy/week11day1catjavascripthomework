d0401d34ca5945f0d49133dd91289d50da62feb9
Clacson <cookiellacson@gmail.com>
2016-12-05 18:23:18 +0000

    app.js created, adding cat

---
+++ public/app.js
@@ -0,0 +1,13 @@
+window.onload = function(){
+
+  var catList = document.createElement('ul');
+  catList.classList.add('cat');
+
+  var catName = document.createElement('li');
+  catName.innerText = "Buddy";
+
+  var catFood = document.createElement('li');
+  catFood.innerText = "bin bags";
+
+  var catPicture = 
+};
