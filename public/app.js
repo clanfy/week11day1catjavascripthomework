ba3325cf14774f9faab2562519376241b588bab1
Clacson <cookiellacson@gmail.com>
2017-01-02 12:44:01 +0000

    successfully appends refactored cat

--- public/app.js
+++ public/app.js
@@ -22,17 +22,55 @@ var appOne = function(){
   catStuff.append(catList);
 };
 
-window.onload = appOne;
-
 var appTwo = function (){
  for (var i = 0; i < CAT_ARRAY.length; i++){
   addCat(CAT_ARRAY[i].title, CAT_ARRAY[i].favoriteFood, CAT_ARRAY[i].img);
  }
 };
 
+var addCat = function(title, favoriteFood, img){
+  var catList = createCatList();
+  var catName = createName(title);
+  var catFood = createFood(favoriteFood);
+  var catPic = createPic(img);
+  appendElements(catList, catName, catFood, catPic);
+};
 
+var appendElements= function(catList, catName, catFood, catPic){
+  var catStuff = document.querySelector('#cats');
+  catList.append(catName);
+  catList.append(catFood);
+  catList.append(catPic);
+  catStuff.append(catList);
+};
+
+
+var createCatList = function(){
+  var catList = document.createElement('ul');
+  catList.classList.add('cat');
+  return catList;
+};
+
+var createName = function(title){
+  var catName = document.createElement('li');
+  catName.innerText = title;
+  return catName;
+};
+
+var createFood = function(favoriteFood){
+  var catFood = document.createElement('li');
+  catFood.innerText = favoriteFood;
+  return catFood;
+};
+
+var createPic = function(pic){
+  var catPic = document.createElement('img');
+  catPic.setAttribute('src', pic);
+  catPic.setAttribute('width', 500);
+  return catPic;
+};
 
 var CAT_ARRAY = [
 {title: "Name: Dennis the Menace", favoriteFood: "Apple Pie", img: "http://www.catbreedsjunction.com/images/burmese-cat-breed-9-lilac-color.jpg"}];
 
-
+window.onload = appTwo;
